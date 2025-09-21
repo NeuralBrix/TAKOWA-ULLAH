5697aa5fec94d6e384cc273d8f752882f97faab6
takowa123 <takowa830@gmail.com>
2025-09-21 20:15:48 +0600

    first commit

--- python_advance/python_decorator.py
+++
@@ -1,3 +0,0 @@
-print("takowa ullah",end=" ")
-name = input("enter your name: ",)
-print(name)
--- test.py
+++
@@ -1,7 +0,0 @@
-class house:
-    def name(self,color):
-        print("its a blueprint")
-
-
-h1 = house()
-h1.name("blue")
